f8eee0ff01a81ae09cdc3f77944dd4177d5aaf1f
fateemabarde086 <fateemabarde086@gmail.com>
2026-07-05 09:02:12 +0100

    Day 2: Installed SQLAlchemy, configured User model, and initialized SQLite database

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configure the SQLite database file location
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database extension
+db = SQLAlchemy(app)
+
+# Define the User Model (Database Schema with correct Capitalization)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    role = db.Column(db.String(50), nullable=False) # admin, doctor, receptionist
+
+    def __repr__(self):
+        return f'<User {self.email} - {self.role}>'
 
 @app.route('/')
 def home():

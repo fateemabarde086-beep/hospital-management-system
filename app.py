a5bba9ae7ef3071685958e0c06d5f4e6c105b0d1
fateemabarde086 <fateemabarde086@gmail.com>
2026-07-08 11:15:08 +0100

    Day 6: Implemented relational Patient database schema model with data integrity constraints

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(50), nullable=False)
+    
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    check_in_status = db.Column(db.Boolean, default=True)
 
 # 1. Update Root Route to route active sessions directly to dashboards
 @app.route('/')
